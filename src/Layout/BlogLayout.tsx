d5014251da9deebb0cca200024bde8c038237cf4
noahsim222 <noahsim222@gmail.com>
2024-10-12 02:17:26 +0900

    add blog page

--- src/Layout/BlogLayout.tsx
+++ src/Layout/BlogLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const BlogLayout = () => {
   return (
     <div className="flex justify-center">
-      <div className="max-w-[1200px] full my-20">
+      <div className="max-w-[1200px] w-full my-20">
         <Outlet />
       </div>
     </div>
