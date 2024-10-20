405a88fdf1387d5e286424bea6a7d685887b6848
noahsim222 <noahsim222@gmail.com>
2024-10-20 18:19:15 +0900

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import HomePage from "@/pages/Home/HomePage";
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
 import { Layout } from "./Layout";
@@ -10,7 +11,6 @@ import FormInput from "./pages/FormInput";
 import ConfirmAlert from "./pages/ConfirmAlert";
 import AboutUs from "./pages/AboutUs";
 import TermsOfService from "./pages/TermsOfService";
-import Blogs from "./components/Blogs";
 import Contact from "./components/Contact";
 import Blog1 from "./blogs/Blog1";
 import Blog7 from "./blogs/Blog7";
@@ -21,6 +21,16 @@ import Blog4 from "./blogs/Blog4";
 import Blog3 from "./blogs/Blog3";
 import Blog2 from "./blogs/Blog2";
 import BlogLayout from "./Layout/BlogLayout";
+import Blog9 from "./blogs/Blog9";
+import Blog10 from "./blogs/Blog10";
+import Blog11 from "./blogs/Blog11";
+import Blog12 from "./blogs/Blog12";
+import Blog13 from "./blogs/Blog13";
+import Blog14 from "./blogs/Blog14";
+import Blog15 from "./blogs/Blog15";
+import Blog16 from "./blogs/Blog16";
+import BlogsPage1 from "./components/Blogs/BlogsPage1";
+import BlogsPage2 from "./components/Blogs/BlogsPage2";
 // import ConfirmAlert1 from "./pages/ConfirmAlert1";
 
 const router = createBrowserRouter([
@@ -37,62 +47,102 @@ const router = createBrowserRouter([
         element: <BlogLayout />,
         children: [
           {
-            path: '/blogs',
-            element: <Blogs />
+            index: true,
+            element: <Navigate to="/blogs/page1" replace />
           },
           {
-            path: '/blogs/1',
+            path: '/blogs/page1',
+            element: <BlogsPage1 />,
+          },
+          {
+            path: '/blogs/page2',
+            element: <BlogsPage2 />,
+          },
+          {
+            path: "/blogs/page1/1",
             element: <Blog1 />
           },
           {
-            path: '/blogs/2',
+            path: "/blogs/page1/2",
             element: <Blog2 />
           },
           {
-            path: '/blogs/3',
+            path: "/blogs/page1/3",
             element: <Blog3 />
           },
           {
-            path: '/blogs/4',
+            path: "/blogs/page1/4",
             element: <Blog4 />
           },
           {
-            path: '/blogs/5',
+            path: "/blogs/page1/5",
             element: <Blog5 />
           },
           {
-            path: '/blogs/6',
+            path: "/blogs/page1/6",
             element: <Blog6 />
           },
           {
-            path: '/blogs/7',
+            path: "/blogs/page1/7",
             element: <Blog7 />
           },
           {
-            path: '/blogs/8',
+            path: "/blogs/page1/8",
             element: <Blog8 />
+          },
+          {
+            path: "/blogs/page1/9",
+            element: <Blog9 />
+          },
+          {
+            path: "/blogs/page1/10",
+            element: <Blog10 />
+          },
+          {
+            path: "/blogs/page2/11",
+            element: <Blog11 />
+          },
+          {
+            path: "/blogs/page2/12",
+            element: <Blog12 />
+          },
+          {
+            path: "/blogs/page2/13",
+            element: <Blog13 />
+          },
+          {
+            path: "/blogs/page2/14",
+            element: <Blog14 />
+          },
+          {
+            path: "/blogs/page2/15",
+            element: <Blog15 />
+          },
+          {
+            path: "/blogs/page2/16",
+            element: <Blog16 />
           },
         ]
       },
       {
-        path:"/formInput",
-        element: <FormInput/>
+        path: "/formInput",
+        element: <FormInput />
       },
       {
-        path:"/confirmAlert",
+        path: "/confirmAlert",
         element: <ConfirmAlert />
       },
       {
-        path:"/about",
-        element:<AboutUs/>
+        path: "/about",
+        element: <AboutUs />
       },
       {
-        path:"/service",
-        element:<TermsOfService/>
+        path: "/service",
+        element: <TermsOfService />
       },
       {
-        path:"/contact",
-        element:<Contact/>
+        path: "/contact",
+        element: <Contact />
       },
       // {
       //   path:"/confirmAlert1",
@@ -103,10 +153,10 @@ const router = createBrowserRouter([
 ]);
 
 const App: FC = () => {
-  
+
   const token = localStorage.getItem("token");
   if (token) {
-      setAuthToken(token);
+    setAuthToken(token);
   }
 
   return (
--- src/Layout/BlogLayout.tsx
+++ src/Layout/BlogLayout.tsx
@@ -1,9 +1,26 @@
 import { Outlet } from "react-router";
+import { NavLink } from "react-router-dom";
 
 const BlogLayout = () => {
   return (
     <div className="flex justify-center">
-      <div className="max-w-[1200px] w-full my-20">
+      <div className="my-20 w-full max-w-[1200px]">
+        <div className="flex justify-end pr-10 mb-10">
+          <NavLink to="/blogs/page1">
+          {
+            ({ isActive }) => (
+              <button className={`${isActive ? 'bg-[#00000044]': 'bg-transparent'} h-10 w-10 border text-center hover:bg-[#00000044] active:scale-75 transition-all origin-center`}>1</button>
+            )
+          }
+          </NavLink>
+          <NavLink to="/blogs/page2">
+          {
+            ({ isActive }) => (
+              <button className={`${isActive ? 'bg-[#00000044]' : 'bg-transparent'} h-10 w-10 border text-center hover:bg-[#00000044] active:scale-75 transition-all origin-center`}>2</button>
+            )
+          }
+          </NavLink>
+        </div>
         <Outlet />
       </div>
     </div>
